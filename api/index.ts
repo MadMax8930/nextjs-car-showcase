ec71ab66cc98ca8563325dbb4c9453995d34a6ca
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-06-21 03:47:33 +0200

    api #2

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,29 @@
+// API #1
 export const fetchCars = async () => {
-
    const headers = {
-      'X-RapidAPI-Key': 'e63c9011cdmshb275bdd29b3dce1p1c9099jsn2a548a1316f1',
+      'X-RapidAPI-Key': `${process.env.NEXT_APP_RAPID_API_KEY}`,
       'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
    }
 
-   const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', { 
+   const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera', { 
       headers: headers
    });
    const result = await response.json();
    return result;
 }
+
+import { CarProps } from "@/types";
+
+// API #2
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+   const url = new URL("https://cdn.imagin.studio/getimage");
+   const { make, model, year } = car;
+ 
+   url.searchParams.append('customer', `${process.env.NEXT_PUBLIC_IMAGIN_API_KEY}` || '');
+   url.searchParams.append('make', make);
+   url.searchParams.append('modelFamily', model.split(" ")[0]);
+   url.searchParams.append('zoomType', 'fullscreen');
+   url.searchParams.append('modelYear', `${year}`);
+   url.searchParams.append('angle', `${angle}`);
+   return `${url}`;
+}; 
